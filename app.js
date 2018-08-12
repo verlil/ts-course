37e35faca4219b518c770a681e6f7670b60f9f67
verlil <vernigoralilia@gmail.com>
2018-08-13 02:20:38 +0300

    forbid compiling in case of errors, generate source map for debugging

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 "use strict";
 var myName = "Lilia";
 var myAge = 24;
-myName = 30; //this will compile to JS despite the fact that this variable is string
+//myName = 30; //this will compile to JS despite the fact that this variable is string
+//myAge = "";
+//# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 let myName: string = "Lilia";
 let myAge: number = 24;
 
-myName = 30; //this will compile to JS despite the fact that this variable is string
+//myName = 30; //this will compile to JS despite the fact that this variable is string
+//myAge = "";
